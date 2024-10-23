352cdafb912a56af78b9e565eaa0e353e670be61
miguelmihm002 <mihm002@yahoo.es>
2024-10-23 16:25:00 +0200

    Add files via upload
    cc

---
+++ bobacon1_1/metadata.py
@@ -0,0 +1,13 @@
+METADATA =\
+{
+	'name': 'bobacon1_1',
+	'description': 'New',
+	'version': '2.6.1',
+	'license': 'MIT',
+	'author': 'New',
+	'url': 'https://bobacon1_1.io'
+}
+
+
+def get(key : str) -> str:
+	return METADATA[key]

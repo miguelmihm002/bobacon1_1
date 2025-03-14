08e9e6d70422c00e5993be7d4f3d11966c7b3e52
miguelmihm002 <mihm002@yahoo.es>
2025-03-14 20:00:51 +0100

    Add files via upload
    c

---
+++ bobacon/uis/components/ui_workflow.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+import gradio
+
+import bobacon
+from bobacon import state_manager, wording
+from bobacon.uis.core import register_ui_component
+
+UI_WORKFLOW_DROPDOWN : Optional[gradio.Dropdown] = None
+
+
+def render() -> None:
+	global UI_WORKFLOW_DROPDOWN
+
+	UI_WORKFLOW_DROPDOWN = gradio.Dropdown(
+		label = wording.get('uis.ui_workflow'),
+		choices = bobacon.choices.ui_workflows,
+		value = state_manager.get_item('ui_workflow'),
+		interactive = True
+	)
+	register_ui_component('ui_workflow_dropdown', UI_WORKFLOW_DROPDOWN)

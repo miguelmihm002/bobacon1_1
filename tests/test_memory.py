775f093ebbd572d6595a644743a9cfeb714c4764
miguelmihm002 <mihm002@yahoo.es>
2025-03-14 19:36:14 +0100

    Delete tests directory
    xxxx

--- tests/test_memory.py
+++
@@ -1,8 +0,0 @@
-from bobacon1_1.common_helper import is_linux, is_macos
-from bobacon1_1.memory import limit_system_memory
-
-
-def test_limit_system_memory() -> None:
-	assert limit_system_memory(4) is True
-	if is_linux() or is_macos():
-		assert limit_system_memory(1024) is False
